571556eedf219b9556fe1f32dba8d405cfe3a1a7
Levin Løssfelt <levin.lossfelt@gmail.com>
2019-04-06 20:05:11 +0200

    Changed title font

--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,48 @@
 import React from "react";
 
 class Book extends React.Component {
-  render () {
+  render() {
     const book = this.props.book;
     return (
       <div>
-      <table>
-    <tr>
-        <td rowSpan="5"><img src={book.picture} alt="bokens forside" heigth="100" width="100" /></td>
-      <tr>
-        <td>Tittel: {book.title}</td>
-        </tr>
-        <tr>
-        <td>Forfatter: {book.author}</td>
-        </tr>
-        <tr>
-        <td>Publisert: {book.published}</td>
+        <table>
+          <tr>
+            <td rowSpan="5">
+              <img
+                src={book.picture}
+                alt="bokens forside"
+                heigth="100"
+                width="100"
+              />
+            </td>
+            <tr>
+              <td>
+                <b>{book.title}</b>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p />
+              </td>
+            </tr>
+            <tr>
+              <td>Forfatter: {book.author}</td>
+            </tr>
+            <tr>
+              <td>Publisert: {book.published}</td>
+            </tr>
+            <tr>
+              <td>Valgt av: {book.chosenBy}</td>
+            </tr>
+            <tr>
+              <td>Antall sider: {book.pages}</td>
+            </tr>
           </tr>
-      <tr>
-        <td>Valgt av: {book.chosenBy}</td>
-          </tr>
-        <tr>
-        <td>Antall sider: {book.pages}</td>
-          </tr>
-        </tr>
         </table>
-      <hr />
-        </div>
+        <hr />
+      </div>
     );
-  };
-};
+  }
+}
 
 export default Book;
