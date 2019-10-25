e085b8a2df1df5b39072f242797f4663e296caf6
Levin Løssfelt <levin.lossfelt@gmail.com>
2019-10-25 10:50:13 +0200

    Graf for gjennomsnittlig antall sider

--- src/Chart.js
+++ src/Chart.js
@@ -4,25 +4,68 @@ import { Bar, Line, Pie } from "react-chartjs-2";
 class Chart extends React.Component {
   render() {
     var books = this.props.books;
-    var bøkene = [];
-    var antallSider = [];
+    var siderPrMedlem = [
+      {
+        Navn: "Levin",
+        totaltSider: 0,
+        antallBøker: 0,
+        gjennomsnittligAntallSider: 0
+      },
+      {
+        Navn: "Øivind",
+        totaltSider: 0,
+        antallBøker: 0,
+        gjennomsnittligAntallSider: 0
+      },
+      {
+        Navn: "Silje",
+        totaltSider: 0,
+        antallBøker: 0,
+        gjennomsnittligAntallSider: 0
+      },
+      {
+        Navn: "Ida",
+        totaltSider: 0,
+        antallBøker: 0,
+        gjennomsnittligAntallSider: 0
+      },
+      {
+        Navn: "Heidi",
+        totaltSider: 0,
+        antallBøker: 0,
+        gjennomsnittligAntallSider: 0
+      }
+    ];
+
     books.forEach(element => {
-      bøkene.push(element.title);
-      antallSider.push(element.pages);
+      siderPrMedlem.forEach(medlem => {
+        if (element.chosenBy === medlem.Navn) {
+          medlem.totaltSider += element.pages;
+          medlem.antallBøker += 1;
+        }
+      });
     });
+    siderPrMedlem.forEach(entry => {
+      entry.gjennomsnittligAntallSider = Math.floor(
+        entry.totaltSider / entry.antallBøker
+      );
+    });
+    //console.log(siderPrMedlem);
 
     var data = {
       labels: [],
       datasets: [
         {
-          label: "Antall sider",
+          label: "Gjennomsnittlig antall sider",
           data: []
         }
       ]
     };
 
-    data.labels = bøkene;
-    data.datasets[0].data = antallSider;
+    siderPrMedlem.forEach(entry => {
+      data.labels.push(entry.Navn);
+      data.datasets[0].data.push(entry.gjennomsnittligAntallSider);
+    });
 
     return (
       <div>
